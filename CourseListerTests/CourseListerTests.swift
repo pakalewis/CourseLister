42570c707b1ff723a1e5c342b67ab0fb64a3bf92
Parker <pakalewis@gmail.com>
2015-03-24 16:10:41 -0700

    Base project created

---
+++ CourseListerTests/CourseListerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CourseListerTests.swift
+//  CourseListerTests
+//
+//  Created by Parker Lewis on 3/24/15.
+//  Copyright (c) 2015 Parker Lewis. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CourseListerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
